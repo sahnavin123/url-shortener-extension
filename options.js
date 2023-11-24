e5b0c86074d74b0176987a04f081e2640c2ff1ec
navin <95286412+sahnavin123@users.noreply.github.com>
2023-11-24 13:37:37 +0530

    added url checking

--- options.js
+++ options.js
@@ -1,21 +1,21 @@
 let saveBtn = document.querySelector("#save");
 let api = document.querySelector("#myapi");
-let toastError = document.querySelector(".toast-error");
-let toastSuccess = document.querySelector(".toast-success");
+let toastError = document.querySelector(".error");
+let toastSuccess = document.querySelector(".success");
 
 const apiKey = "bW7wpegbjeGgFArtPdj8ERAn2Oj3iD2jNWy5gqwx2UHCJiAb9zHWIkz0d0fq";
 
 saveBtn.addEventListener("click", () => {
     const api_value = api.value ? api.value : apiKey;
   if (api_value) {
-    toastSuccess.classList.remove("d-hide");
+    toastSuccess.classList.remove("hide");
     chrome.storage.local.set({ API: api_value }, function () {
       console.log("Value is set to " + api_value);
     });
   } else {
-    toastError.classList.remove("d-hide");
+    toastError.classList.remove("hide");
     setTimeout(() => {
-      toastError.classList.add("d-hide");
+      toastError.classList.add("hide");
     }, 1500);
   }
 });
--- popup.js
+++ popup.js
@@ -1,16 +1,26 @@
 let generateBtn = document.querySelector("#shortURL");
 let api = document.querySelector("#myurl");
-let toastError = document.querySelector(".toast-error");
-let toastSuccess = document.querySelector(".toast-success");
+let toastError = document.querySelector(".error");
+let toastSuccess = document.querySelector(".success");
 let loader = document.querySelector(".loading");
 const url = new URL("https://t.ly/api/v1/link/shorten");
 let headers = {
   "Content-Type": "application/json",
   Accept: "application/json",
 };
+
+function isValidURL(inputURL) {
+  try {
+    new URL(inputURL);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 generateBtn.addEventListener("click", () => {
-  if (api.value) {
-    loader.classList.remove("d-hide");
+  if (api.value && isValidURL(api.value)) {
+    loader.classList.remove("hide");
     chrome.storage.local.get(["API"], async function (result) {
       try {
         let apiToken = result.API
@@ -28,15 +38,18 @@ generateBtn.addEventListener("click", () => {
 
         const json = await response.json();
 
-        loader.classList.add("d-hide");
-        toastSuccess.classList.remove("d-hide");
+        loader.classList.add("hide");
+        toastSuccess.classList.remove("hide");
 
         const link = document.createElement("a");
         link.href = json.short_url;
         link.textContent = json.short_url;
         link.style.color = "green";
+        const text = document.createElement("p");
+        text.innerHTML = "Generated url: ";
 
         toastSuccess.innerHTML = "";
+        toastSuccess.appendChild(text);
         toastSuccess.appendChild(link);
         link.target = "_blank";
       } catch (err) {
@@ -44,9 +57,9 @@ generateBtn.addEventListener("click", () => {
       }
     });
   } else {
-    toastError.classList.remove("d-hide");
+    toastError.classList.remove("hide");
     setTimeout(() => {
-      toastError.classList.add("d-hide");
+      toastError.classList.add("hide");
     }, 1500);
   }
 });
